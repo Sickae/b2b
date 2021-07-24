18a4d968579d2c9061db45b5c7d8cf59db01bbe5
Sipos Bence <sipos.bnc@gmail.com>
2021-07-24 23:58:42 +0200

    application test data updated

--- Src/B2B.DatabaseCreator/TestDataSetup/ApplicationTestData.cs
+++ Src/B2B.DatabaseCreator/TestDataSetup/ApplicationTestData.cs
@@ -23,9 +23,9 @@ namespace B2B.DatabaseCreator.TestDataSetup
                 {
                     new ApplicationFlowQuestion
                     {
-                        Code = "first_question",
+                        Code = "class",
                         Type = ApplicationFlowQuestionType.Text,
-                        Text = "First question"
+                        Text = "What class are you playing as your main?"
                     },
                     new ApplicationFlowQuestion
                     {
@@ -34,28 +34,62 @@ namespace B2B.DatabaseCreator.TestDataSetup
                         Text = "Second question",
                         Choices = new ApplicationFlowQuestionChoice[]
                         {
-                            new() {Value = "Option 1", Ordinal = 0},
-                            new() {Value = "Option 2", Ordinal = 1},
-                            new() {Value = "Option 3", Ordinal = 2}
+                            new() {Value = "Blader", Ordinal = 0},
+                            new() {Value = "Wizard", Ordinal = 1},
+                            new() {Value = "Warrior", Ordinal = 2},
+                            new() {Value = "Gladiator", Ordinal = 2},
+                            new() {Value = "Force Shielder", Ordinal = 3},
+                            new() {Value = "Force Archer", Ordinal = 4},
+                            new() {Value = "Force Gunner", Ordinal = 5},
+                            new() {Value = "Force Blader", Ordinal = 6},
+                            new() {Value = "Dark Mage", Ordinal = 7},
                         }
                     },
                     new ApplicationFlowQuestion
                     {
-                        Code = "third_question",
-                        Type = ApplicationFlowQuestionType.MultiChoice,
-                        Text = "Third question",
+                        Code = "play_time",
+                        Type = ApplicationFlowQuestionType.Choice,
+                        Text = "How long have you been playing Cabal Online?",
                         Choices = new ApplicationFlowQuestionChoice[]
                         {
-                            new() {Value = "Option 1", Ordinal = 0},
-                            new() {Value = "Option 2", Ordinal = 1},
-                            new() {Value = "Option 3", Ordinal = 2}
+                            new() {Value = "Less than 1 year", Ordinal = 0},
+                            new() {Value = "1-5 years", Ordinal = 1},
+                            new() {Value = "5-10 years", Ordinal = 2},
+                            new() {Value = "More than 10 years", Ordinal = 3}
                         }
                     },
                     new ApplicationFlowQuestion
                     {
-                        Code = "last_optional_question",
+                        Code = "source",
+                        Type = ApplicationFlowQuestionType.MultiChoice,
+                        Text = "Why would you like to join us?",
+                        Choices = new ApplicationFlowQuestionChoice[]
+                        {
+                            new() {Value = "For the dungeons", Ordinal = 0},
+                            new() {Value = "For the community", Ordinal = 1},
+                            new() {Value = "Can't find any other guild", Ordinal = 2},
+                            new() {Value = "To scam", Ordinal = 3},
+                        }
+                    },
+                    new ApplicationFlowQuestion
+                    {
+                        Code = "likely_to_quit",
+                        Type = ApplicationFlowQuestionType.Choice,
+                        Text = "How likely are you to quit or take a break from the game in the next 3 months?",
+                        Choices = new ApplicationFlowQuestionChoice[]
+                        {
+                            new() {Value = "Definitely not", Ordinal = 0},
+                            new() {Value = "Not likely", Ordinal = 1},
+                            new() {Value = "Likely", Ordinal = 2},
+                            new() {Value = "Guaranteed", Ordinal = 3},
+                        },
+                        IsInlineChoice = true,
+                    },
+                    new ApplicationFlowQuestion
+                    {
+                        Code = "about_yourself",
                         Type = ApplicationFlowQuestionType.Text,
-                        Text = "Optional question",
+                        Text = "Write something about yourself.",
                         IsOptional = true
                     }
                 }
