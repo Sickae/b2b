223d8647879e73d4b8730c59cbc21fea1c30d364
Sipos Bence <sipos.bnc@gmail.com>
2021-07-25 16:42:40 +0200

    site-config taghelper added

---
+++ Src/B2B.Web/TagHelpers/SiteConfigTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace B2B.Web.TagHelpers
+{
+    [HtmlTargetElement("site-config", Attributes = "key,value", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class SiteConfigTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            context.AllAttributes.TryGetAttribute("key", out var key);
+            context.AllAttributes.TryGetAttribute("value", out var value);
+
+            output.Attributes.Clear();
+
+            output.TagName = "input";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.Add("value", $"cfg-{key.Value}_{value.Value}");
+            output.Attributes.Add("hidden", "hidden");
+        }
+    }
+}
