a7f9f91b0286315e22bc2e5195cd386947949bdc
Sipos Bence <sipos.bnc@gmail.com>
2021-07-25 02:23:18 +0200

    input taghelper added

---
+++ Src/B2B.Shared/Attributes/PlaceholderAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace B2B.Shared.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PlaceholderAttribute : Attribute
+    {
+        public string Placeholder { get; set; }
+
+        public PlaceholderAttribute(string placeholder = null)
+        {
+            Placeholder = placeholder;
+        }
+    }
+}
---
+++ Src/B2B.Web/TagHelpers/InputTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Reflection;
+using B2B.Shared.Attributes;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace B2B.Web.TagHelpers
+{
+    [HtmlTargetElement("input", Attributes = AttributeName, TagStructure = TagStructure.WithoutEndTag)]
+    public class InputTagHelper : TagHelper
+    {
+        private const string AttributeName = "asp-placeholder";
+
+        [HtmlAttributeName(AttributeName)]
+        public ModelExpression For { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var placeholder = For.Name;
+            var placeholderAttr = For.Metadata?.ContainerType?.GetProperty(For.Name)?
+                .GetCustomAttribute<PlaceholderAttribute>();
+
+            if (placeholderAttr != null && output.Attributes.All(x => x.Name != "placeholder"))
+                placeholder = placeholderAttr.Placeholder ?? For.Name;
+
+            output.Attributes.Add("placeholder", placeholder);
+        }
+    }
+}
